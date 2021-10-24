fafd7b88c3274d0ef4a3a778323b7c398df0184d
MuhammdinKs <85553284+Muhammedciks@users.noreply.github.com>
2021-10-24 12:01:04 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const Discord = require("discord.js");
+const Enmap = require("enmap");
+const config = require("./config.json")
+
+const client = new Discord.Client({
+    shards: "auto", // 1800+ ,
+    allowedMentions: {
+      parse: ["roles", "users"],
+      repliedUser: false,
+    },
+    failIfNotExists: false,
+    presence: {
+      activity: {
+        name: `Dm me for help | m!help`, 
+        type: "PLAYING", 
+        url: "https://twitch.tv/#"
+      },
+      status: "online"
+    },
+    restTimeOffset: 0,
+    partials: ["CHANNEL", "MESSAGE", "REACTION"],
+    intents: [
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MEMBERS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES,
+        Discord.Intents.FLAGS.DIRECT_MESSAGES,
+    ]
+});
+client.modmailDb = new Enmap({
+    name: "Modmail-Database",
+    //dataDir: "./databases/modmail" //create the folder(s)
+});
+client.settings = new Enmap({
+    name: "Settings-Database",
+    //dataDir: "./databases/settings" //create the folder(s)
+});
+client.login("ODk1MzM4NjY3NTQ2MTQ4OTE0.YV3HHA.hEb3ZRkKMe84X6fEZgFN9Jx9ENA")
+client.on("ready", () => {
+    console.log(`${client.user.tag} is now online!`);
+})
+
+require("./modmail")(client);
